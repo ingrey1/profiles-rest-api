67847386678bee65d39391d02b6e611530c6506b
Jason Mortensen <ingrey1@gmail.com>
2019-07-02 14:43:10 -0700

    added hello-viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,65 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 from profiles_api import serializers
 
 # Create your views here.
+
+
+class HelloViewset(viewsets.ViewSet):
+    """test api viewset"""
+
+    serializer_class = serializers.HelloSerializer
+
+  
+    def list(self, request):
+        """return a hello message"""
+
+        a_viewset = ['uses actions (create, list, retrieve, update, partial_update', 'provides more functionality with less code']
+
+
+
+
+        return Response({'message': 'hello', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        """Create a new hello message"""
+
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+
+            name = serializer.validated_data.get('name')
+            return Response({'message': f'Hello {name}'})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its id"""
+
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handle updating an object by replacing whole object with new one"""
+
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """handle updating part of an object"""
+
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """handle removing an object"""
+
+        return Response({'http_method': 'DELETE'})
+
+
+
+
+
+
 
 class HelloApiView(APIView):
     """Test API View"""
