fb0582307f53936522b76861b8d03099cd325f86
Jason Mortensen <ingrey1@gmail.com>
2019-07-05 07:21:05 -0700

    Added token based authentication for the profile api endpoint

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -7,8 +7,15 @@ from profiles_api import serializers
 from profiles_api import models
 from profiles_api import permissions
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 # Create your views here.
+
+class UserLoginApiView(ObtainAuthToken):
+    """creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
@@ -18,7 +25,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
-    search_fields = ('name', 'email',)
+    search_fields = ('name', 'email',)  
 
 class HelloViewset(viewsets.ViewSet):
     """test api viewset"""
