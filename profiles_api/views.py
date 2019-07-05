71e13a75cb2d34cb095c077d70bf2700d3fb7dd7
Jason Mortensen <ingrey1@gmail.com>
2019-07-05 06:51:28 -0700

    Added search functionality to /api/profile/ endpoint

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework import filters
 from profiles_api import serializers
 from profiles_api import models
 from profiles_api import permissions
@@ -16,7 +17,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
-
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
 
 class HelloViewset(viewsets.ViewSet):
     """test api viewset"""
